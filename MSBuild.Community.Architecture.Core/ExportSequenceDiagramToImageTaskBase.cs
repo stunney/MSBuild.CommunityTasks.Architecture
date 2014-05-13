a79c846b0587b6a10c6df726d954a1b4eba7ed75
stunney <s_tunney2000@yahoo.com>
2014-05-12 23:26:58 -0400

    Updating to include support for VS2012
    Now have two projects, one for 2012 (11.0) and one for 2013 (12.0) that
    are versioned to match the VS version and the version of the SDK
    assemblies that are required to have this function properly.  Nuget
    definitions for each as well.  Still waiting on NuGet pull request to be
    honoured.  For now simply changing the GUID in your sln file for your
    modelproj project to the CSPROJ guid value will allow you to use this
    msbuild task inline.

---
+++ MSBuild.Community.Architecture.Core/ExportSequenceDiagramToImageTaskBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Collections;
+
+namespace MSBuild.Community.Architecture.Core
+{
+    public abstract class ExportSequenceDiagramToImageTaskBase : Task
+    {
+        [Required]
+        public virtual string ModelingProjectPath { get; set; }
+
+        [Required]
+        public virtual string OutputPath { get; set; }
+
+        //Doing this IDisposable crap because that is the ONLY common base interface for both 2012 and 2013 SDK versions of the classes.
+        //They don't derive from ANYTHING else :(
+
+        //Strong assumptions, I know.  But hey, I wrote it ;)
+
+        protected abstract IDisposable LoadModelingProject(string modelingProjectPath);
+        protected abstract Metafile GetEntireImageMetafileFromDiagramFile(string diagramFilename, IDisposable modelingProject);
+        protected abstract IEnumerable<string> GetModelingProjectFilenames(IDisposable modelingProject);
+
+        protected virtual void SaveMetafileToFile(System.Drawing.Imaging.Metafile image, string output)
+        {
+            using (Stream s = new FileStream(output, FileMode.Create))
+            {
+                Log.LogMessage("Saving image as jpg to {0}", output);
+                image.Save(s, ImageFormat.Jpeg);
+            }
+        }
+
+        public override bool Execute()
+        {
+            //Visual studio requires this in order to execute this task! http://www.microsoft.com/en-us/download/confirmation.aspx?id=40754
+
+            bool retval = false;
+
+            try
+            {
+                Log.LogMessage("Loading modeling project {0}", ModelingProjectPath);
+
+                IDisposable proj = LoadModelingProject(ModelingProjectPath);
+
+                foreach (string diagramFilename in GetModelingProjectFilenames(proj))
+                {
+                    Log.LogMessage("Loading diagram file {0}", diagramFilename);
+                    string output = string.Concat(OutputPath, System.IO.Path.DirectorySeparatorChar, new FileInfo(diagramFilename).Name, @".jpg");
+                    SaveMetafileToFile(GetEntireImageMetafileFromDiagramFile(diagramFilename, proj), output);
+                }
+                Log.LogMessage("Done");
+
+                retval = true;
+            }
+            catch (Exception _ex)
+            {
+                Log.LogError(_ex.Message);
+            }
+
+            return retval;
+        }
+    }
+}
--- MSBuild.Community.Architecture.Tasks.11.0/ExportSequenceDiagramToImageTask.cs
+++ MSBuild.Community.Architecture.Tasks.11.0/ExportSequenceDiagramToImageTask.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.IO;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
-
-using Microsoft.VisualStudio.Modeling.Diagrams;
-using Microsoft.VisualStudio.ArchitectureTools.Extensibility;
-using Microsoft.VisualStudio.ArchitectureTools.Extensibility.Presentation;
+using System.Text;
+using System.Threading.Tasks;
 
 using MSBuild.Community.Architecture.Core;
 
-namespace MSBuild.Community.Architecture.Tasks._12._0
+using Microsoft.VisualStudio.ArchitectureTools.Extensibility;
+using Microsoft.VisualStudio.ArchitectureTools.Extensibility.Presentation;
+using Microsoft.VisualStudio.Modeling.Diagrams;
+using Microsoft.Build.Framework;
+
+namespace MSBuild.Community.Architecture.Tasks._11._0
 {
-    public class ArchitectureDiagramsToImagesTask : ExportSequenceDiagramToImageTaskBase, ITask
+    public class ExportSequenceDiagramToImageTask : ExportSequenceDiagramToImageTaskBase, ITask
     {
-        //http://blogs.msdn.com/b/oscar_calvo/archive/2010/05/24/how-to-display-modeling-diagrams-outside-visual-studio.aspx
-        //http://msdn.microsoft.com/en-us/library/ff469815.aspx
-        //http://stackoverflow.com/questions/3009163/a-generic-error-occurred-in-gdi
-
-        //Visual studio requires this in order to execute this task! http://www.microsoft.com/en-us/download/confirmation.aspx?id=40754
-
         protected override IDisposable LoadModelingProject(string modelingProjectPath)
         {
             IModelingProjectReader proj = ModelingProject.LoadReadOnly(modelingProjectPath);
@@ -33,7 +29,7 @@ namespace MSBuild.Community.Architecture.Tasks._12._0
             Log.LogMessage("Diagram loaded into interface.");
             Diagram d = diag.GetObject<Diagram>();
             ICollection selectedShapes = new[] { diag.GetObject<PresentationElement>() };
-            return d.CreateMetafile(selectedShapes);
+            return d.CreateMetafile(selectedShapes);            
         }
 
         protected override IEnumerable<string> GetModelingProjectFilenames(IDisposable modelingProject)
